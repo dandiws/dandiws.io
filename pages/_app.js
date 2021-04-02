212de7292cf0fa7c589c13aba8619ad731b0fec4
Dandi Wiratsangka <dandiwiratsangka@gmail.com>
2021-04-02 18:20:17 +0700

    add prism light css

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,13 @@ import Layout from '../components/Layout'
 import { MDXProvider } from '@mdx-js/react'
 import mdxcomponents from '../utils/mdxcomponents'
 import theme from '../theme'
-import prism from '../styles/prism'
 import { ChakraProvider } from '@chakra-ui/react'
-import { Global } from '@emotion/react'
 
 function MyApp ({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme} resetCSS>
       <MDXProvider components={mdxcomponents}>
         <Layout>
-          <Global styles={prism} />
           <Component {...pageProps} />
         </Layout>
       </MDXProvider>
