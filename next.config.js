59460f245df524fc4954cc1c35689d2449b81b58
Dandi Wiratsangka <dandiwiratsangka@gmail.com>
2022-01-27 10:45:58 +0700

    add bundle analyzer and remove dependencies

--- next.config.js
+++ next.config.js
@@ -1,33 +1,7 @@
-// const withMdxEnhanced = require('next-mdx-enhanced')
-// const remarkCodeTitles = require('remark-code-titles')
-// const rehypeMdxPrism = require('mdx-prism')
-// const readingTime = require('reading-time')
-// const dayjs = require('dayjs')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
 
-// module.exports = withMdxEnhanced({
-//   layoutPath: 'layouts',
-//   defaultLayout: true,
-//   fileExtensions: ['mdx'],
-//   remarkPlugins: [remarkCodeTitles],
-//   rehypePlugins: [rehypeMdxPrism],
-//   usesSrc: false,
-//   extendFrontMatter: {
-//     process: (mdxContent, frontMatter) => {
-//       return {
-//         read_time: readingTime(mdxContent),
-//         published_at:
-//           frontMatter.published_at && dayjs(frontMatter.published_at).valueOf(),
-//         published_at_formatted:
-//           frontMatter.published_at &&
-//           dayjs(frontMatter.published_at).format('DD MMMM YYYY')
-//       }
-//     }
-//     // phase: 'prebuild',
-//   },
-//   reExportDataFetching: false,
-//   swcMinify: true
-// })()
-
-module.exports = {
+module.exports = withBundleAnalyzer({
   swcMinify: true
-}
+})
